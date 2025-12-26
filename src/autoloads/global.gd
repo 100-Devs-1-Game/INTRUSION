7ae0a190406a4a7905f1cc3e0e9b246c4357d5ab
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-27 00:36:23 +0100

    Hook up audio settings
    Add audio busses for music and sfx
    Settings slider now modify bus volumes and values get stored
    Audioplayers still need to be set to Sfx/Music
    Fullscreen checkbox isn't hooked up yet

--- src/autoloads/global.gd
+++ src/autoloads/global.gd
@@ -1,8 +1,58 @@
-class_name global extends Node
+extends Node
 
-var master_audio_level: float = 0.13
-var sound_effects_level: float = 0.13
-var music_level: float = 0.13
+const SAVE_FILE= "user://settings.cfg"
 
-func _init() -> void:
-	pass
+var master_audio_level: int:
+	set(i):
+		master_audio_level= i
+		AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), i / 100.0)
+
+var sound_effects_level: int:
+	set(i):
+		sound_effects_level= i
+		AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Sfx"), i / 100.0)
+
+var music_level: int:
+	set(i):
+		music_level= i
+		AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), i / 100.0)
+
+var fullscreen: bool:
+	set(b):
+		fullscreen= b
+		if OS.get_name() == "Web":
+			return
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if b else\
+			DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+
+func _ready() -> void:
+	master_audio_level= 50
+	sound_effects_level= 50
+	music_level= 50
+	fullscreen= false
+
+	load_settings()
+
+
+func save_settings():
+	var config = ConfigFile.new()
+
+	config.set_value("Audio", "master_volume", master_audio_level)
+	config.set_value("Audio", "sfx_volume", sound_effects_level)
+	config.set_value("Audio", "music_volume", music_level)
+	config.set_value("Misc", "fullscreen", fullscreen)
+
+	config.save(SAVE_FILE)
+
+
+func load_settings():
+	var config = ConfigFile.new()
+	if config.load(SAVE_FILE) != OK:
+		return
+
+	master_audio_level= config.get_value("Audio", "master_volume", 0.7)
+	music_level= config.get_value("Audio", "music_volume", 0.7)
+	sound_effects_level= config.get_value("Audio", "sfx_volume", 0.7)
+	fullscreen= config.get_value("Misc", "fullscreen")
--- src/scenes/settings.gd
+++ src/scenes/settings.gd
@@ -4,34 +4,35 @@ class_name settings extends Control
 @export var SFXAUDIO : HSlider
 @export var MUSICAUDIO : HSlider
 @export var INGAME : bool
+
+
+
 func _ready() -> void:
 	setLocals()
-	pass
-	
+
 
 func setGlobals() -> void:
-	Global.master_audio_level = MASTERAUDIO.value/100
-	Global.sound_effects_level = SFXAUDIO.value/100
-	Global.music_level = MUSICAUDIO.value/100
+	Global.master_audio_level = MASTERAUDIO.value
+	Global.sound_effects_level = SFXAUDIO.value
+	Global.music_level = MUSICAUDIO.value
+
 
 func setLocals() -> void:
-	MASTERAUDIO.value = Global.master_audio_level * 100
-	SFXAUDIO.value = Global.sound_effects_level * 100
-	MUSICAUDIO.value = Global.music_level * 100
+	MASTERAUDIO.value = Global.master_audio_level
+	SFXAUDIO.value = Global.sound_effects_level
+	MUSICAUDIO.value = Global.music_level
+
 
 func _on_master_value_changed(value: float) -> void:
 	print("changing master audio level to: " + str(value))
-	Global.master_audio_level = value/100
-	pass # Replace with function body.
+	Global.master_audio_level = value
 
 
 func _on_sfx_value_changed(value: float) -> void:
 	print("changing sfx audio level to: " + str(value))
-	Global.sound_effects_level = value/100
-	pass # Replace with function body.
+	Global.sound_effects_level = value
 
 
 func _on_music_value_changed(value: float) -> void:
 	print("changing music audio level to: " + str(value))
-	Global.music_level = value/100
-	pass # Replace with function body.
+	Global.music_level = value
